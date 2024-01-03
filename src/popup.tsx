a0922c5f0f87649806a0b1e712239cc0770a57fb
RolandWarburton <warburtonroland@gmail.com>
2024-01-04 10:43:16 +1100

    Use chrome local storage to remember the last set password

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,13 +1,22 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { genpw } from '@rolandwarburton/pwgen';
 
 const App = () => {
   const [password, setPassword] = useState('');
 
   useEffect(() => {
-    (async () => {
-      setPassword(await genpw());
-    })();
+    // get the password from storage (set if its not stored)
+    chrome.storage.local.get('password', async (result) => {
+      if (Object.keys(result).length !== 0) {
+        setPassword(result.password);
+      } else {
+        const newPassword = await genpw();
+        localStorage.setItem('key', 'value');
+        chrome.storage.local.set({ password: newPassword }, () => {
+          setPassword(newPassword);
+        });
+      }
+    });
   }, []);
 
   return <div>{password}</div>;
