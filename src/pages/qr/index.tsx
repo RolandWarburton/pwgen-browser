b708b4925a530f52585e175efb15e25745d745f2
RolandWarburton <warburtonroland@gmail.com>
2024-01-09 17:43:56 +1100

    Custom backlink for qr codes as this route will be nested

--- src/pages/qr/index.tsx
+++ src/pages/qr/index.tsx
@@ -3,14 +3,18 @@ import { QRCodeSVG } from 'qrcode.react';
 import { Link } from 'react-router-dom';
 import { Button, Container, Row } from '../../components/styles';
 import { useParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 function PasswordQRCode() {
   const { param } = useParams();
+  const [searchParams, _] = useSearchParams();
+  const back = searchParams.get('back');
+  console.log(back);
   return (
     <div>
       <Container>
         <Row columns="1fr">
-          <Link to="/">
+          <Link to={`/${back || ''}`}>
             <Button>Back</Button>
           </Link>
         </Row>
