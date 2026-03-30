a7143875e636eac0b00b0e4cda58e1035b169427
RolandWarburton <warburtonroland@gmail.com>
2026-03-31 08:37:13 +1100

    Bump version to 1.0.2

--- scripts/release.js
+++ scripts/release.js
@@ -3,10 +3,10 @@ import { existsSync } from 'fs';
 import { join } from 'path';
 
 // --- Configuration ---
-const TAG = 'v1.0.1';
-const RELEASE_NAME = 'Version 1.0.1';
+const TAG = 'v1.0.2';
+const RELEASE_NAME = 'Version 1.0.2';
 const ZIP_FILE_PATH = 'pwgen-browser.zip';
-const RELEASE_NOTES = 'Minor bugfix';
+const RELEASE_NOTES = 'Put infrequent buttons into ellipsis dropdown';
 // ---------------------
 
 function run(command) {
